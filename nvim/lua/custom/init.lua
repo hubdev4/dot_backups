e3f2314f94d8fbefedd921acefb76485b04a1ff2
Hub <hubdev4@gmail.com>
2022-07-08 20:24:28 -0400

    configs

---
+++ nvim/lua/custom/init.lua
@@ -0,0 +1,13 @@
+local set = vim.opt
+
+set.rnu = true
+--g.hi.normal.guibg = 000000
+--vim.cmd("hi normal guibg=000000")
+--vim.api.nvim_command('hi normal guibg=000000')
+--vim.cmd[[ set guicursor= ]]
+--vim.cmd[[ hi normal guibg=000000 ]]
+--return {
+--
+--  ["dart-lang/dart-vim-plugin"] = {},
+--}
+
